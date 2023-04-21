7bd947486984a3371fa96a4e6b3fdd59a5424f76
Gustavo Ayala <gustavoayala25@gmail.com>
2023-04-20 23:21:47 -0300

    ejercicio uno

---
+++ uno.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char nom[10];
+    int edad;
+    printf("Ingrese su nombre y edad: ");
+    scanf("%s %d", nom, &edad);
+    printf("Su nombre es: %s \n", nom);
+    printf("Su edad es: %d \n", edad);
+    return 0;
+}
